a47327711962397759640cd33a31c3f0478e0a1c
Nelson Steve <nelsonsteve799@gmail.com>
2022-10-23 22:54:53 +0500

    Added assertion defines

--- Albedo/src/Albedo/Core.h
+++ Albedo/src/Albedo/Core.h
@@ -17,6 +17,18 @@
 
 #endif
 
+#ifdef Albedo_DEBUG
+#define Albedo_ENABLE_ASSERTS
+#endif
+
+#ifdef Albedo_ENABLE_ASSERTS
+#define Albedo_ASSERT(x, ...) { if(!(x)) { HZ_ERROR("Assertion Failed: {0}", __VA_ARGS__); __debugbreak(); } }
+#define Albedo_CORE_ASSERT(x, ...) { if(!(x)) { HZ_CORE_ERROR("Assertion Failed: {0}", __VA_ARGS__); __debugbreak(); } }
+#else
+#define Albedo_ASSERT(x, ...)
+#define Albedo_CORE_ASSERT(x, ...)
+#endif
+
 #define BIT(x) (1 << x)
 
 #define BIND_EVENT_FN(x) std::bind(&Application::x, this, std::placeholders::_1)
